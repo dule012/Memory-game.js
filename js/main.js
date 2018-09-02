71141637dfb117532f72eff8ae60009e58a0df8a
BIT student <dusanmihajlovic012@gmail.com>
2018-09-02 23:00:25 +0200

    css responsive progress

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-listOfCardTypes = [
-    "fa fa-diamond",
-    "fa fa-paper-plane-o",
-    "fa fa-anchor",
-    "fa fa-bolt",
-    "fa fa-cube",
-    "fa fa-leaf",
-    "fa fa-bicycle",
-    "fa fa-bomb"
-]
+let label = document.querySelectorAll('label')
+
+for (let i = 0; i < label.length; i++) {
+    ((i) => {
+        label[i].addEventListener('click',()=>{
+            console.log(label[i].firstElementChild.value)
+        })
+    })(i)
+}
