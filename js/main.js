f98010641eb41f625a7bb4b6d3aea281b4628f8e
BIT student <dusanmihajlovic012@gmail.com>
2018-09-03 00:54:56 +0200

    css progress

--- js/main.js
+++ js/main.js
@@ -2,8 +2,20 @@ let label = document.querySelectorAll('label')
 
 for (let i = 0; i < label.length; i++) {
     ((i) => {
-        label[i].addEventListener('click',()=>{
+        label[i].addEventListener('click', () => {
             console.log(label[i].firstElementChild.value)
         })
+    })(i)
+}
+
+let cards = document.querySelectorAll('.card3')
+
+for (let i = 0; i < cards.length; i++) {
+    ((i) => {
+
+        cards[i].addEventListener('click', () => {
+            cards[i].style.animation = 'same 1.4s linear '
+        })
+
     })(i)
 }
